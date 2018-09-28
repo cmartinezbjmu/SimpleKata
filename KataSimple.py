4842fa7263930223a11fa409fec18cf495eaba76
Cristian Martinez <tura182@gmail.com>
2018-09-28 16:53:30 -0500

    Paso 5- Se corrige metodo para que devuelva el resultado en un arreglo (Verde)

--- KataSimple.py
+++ KataSimple.py
@@ -1,13 +1,14 @@
-
+from numpy import *
 
 class KataSimple:
     def contar(self,cadena):
         if cadena == "":
-            return 0
+            return [0]
         else:
             cadena = cadena.replace(":",",")
             numeros = cadena.split(",")
             cantidad = 0
             for caracter in numeros:
                 cantidad = cantidad + 1
-            return cantidad
+            arreglo = array([cantidad])
+            return arreglo
--- KataSimpleTest.py
+++ KataSimpleTest.py
@@ -4,16 +4,16 @@ from KataSimple import KataSimple
 
 class KataSimpleTest(TestCase):
     def test_contar(self):
-        self.assertEqual(KataSimple().contar(""),0,"Cadena vacia")
+        self.assertEqual(KataSimple().contar(""),[0],"Cadena vacia")
 
     def test_contar_numero(self):
-        self.assertEqual(KataSimple().contar("1"),1,"Contar un numero")
+        self.assertEqual(KataSimple().contar("1"),[1],"Contar un numero")
 
     def test_contar_tresNumeros(self):
-        self.assertEqual(KataSimple().contar("1,2,3"),3,"Contar tres numeros")
+        self.assertEqual(KataSimple().contar("1,2,3"),[3],"Contar tres numeros")
 
     def test_contar_N_Numeros(self):
-        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),8, "Contar con diferentes separadores")
+        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8], "Contar con diferentes separadores")
 
     def test_contar_N_Numeros_retornarArreglo(self):
         self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8], "Contar numeros y retornar un arreglo con la cantidad")
