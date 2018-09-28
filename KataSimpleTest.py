cde7beecfa8c4017cb8d94a6d266cc0dfae7b8d5
michel <michelini19a@gmail.com>
2018-09-28 15:54:47 -0500

    Paso 3-Se crea test para la Contar tres numeros separados por coma(Rojo)

--- KataSimpleTest.py
+++ KataSimpleTest.py
@@ -7,5 +7,8 @@ class KataSimpleTest(TestCase):
         self.assertEqual(KataSimple().contar(""),0,"Cadena vacia")
 
     def test_contar_numero(self):
-        self.assertEqual(KataSimple().contar("1"),1,"Cadena vacia")
+        self.assertEqual(KataSimple().contar("1"),1,"Contar un numero")
+
+    def test_contar_tresNumeros(self):
+        self.assertEqual(KataSimple().contar("1,2,3"),3,"Contar tres numeros")
 
