e7bdb8f997ea5106cd7be54aea95c338c06e3d0e
Cristian Martinez <tura182@gmail.com>
2018-09-28 17:14:38 -0500

    Paso 6- Se corrige metodo para que la cantidad y el menor numero del arreglo (Verde)

--- KataSimple.py
+++ KataSimple.py
@@ -3,12 +3,15 @@ from numpy import *
 class KataSimple:
     def contar(self,cadena):
         if cadena == "":
-            return [0]
+            return [0,0,0,0]
         else:
             cadena = cadena.replace(":",",")
             numeros = cadena.split(",")
             cantidad = 0
+            menor = numeros[0]
             for caracter in numeros:
                 cantidad = cantidad + 1
-            arreglo = array([cantidad])
+                if caracter < menor:
+                    menor = caracter
+            arreglo = [cantidad,int(menor),0,0]
             return arreglo
--- KataSimpleTest.py
+++ KataSimpleTest.py
@@ -4,21 +4,21 @@ from KataSimple import KataSimple
 
 class KataSimpleTest(TestCase):
     def test_contar(self):
-        self.assertEqual(KataSimple().contar(""),[0],"Cadena vacia")
+        self.assertEqual(KataSimple().contar(""),[0,0,0,0],"Cadena vacia")
 
     def test_contar_numero(self):
-        self.assertEqual(KataSimple().contar("1"),[1],"Contar un numero")
+        self.assertEqual(KataSimple().contar("1"),[1,1,0,0],"Contar un numero")
 
     def test_contar_tresNumeros(self):
-        self.assertEqual(KataSimple().contar("1,2,3"),[3],"Contar tres numeros")
+        self.assertEqual(KataSimple().contar("1,2,3"),[3,1,0,0],"Contar tres numeros")
 
     def test_contar_N_Numeros(self):
-        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8], "Contar con diferentes separadores")
+        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8,1,0,0], "Contar con diferentes separadores")
 
     def test_contar_N_Numeros_retornarArreglo(self):
-        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8], "Contar numeros y retornar un arreglo con la cantidad")
+        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8,1,0,0], "Contar numeros y retornar un arreglo con la cantidad")
 
     def test_cantNum_Menor(self):
-        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8,1], "Contar numeros y calcular el menor")
+        self.assertEqual(KataSimple().contar("1,2:3,4:6,7,8,9"),[8,1,0,0], "Contar numeros y calcular el menor")
 
 
